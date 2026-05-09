7a52720f2fed7b5cc4e3cdc22e14412266cd5c63
yousefAtef2 <dev.yousef.atef@gmail.com>
2026-05-09 09:14:26 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/portfolio2/',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
@@ -16,3 +16,4 @@ export default defineConfig({
     },
   },
 });
+
